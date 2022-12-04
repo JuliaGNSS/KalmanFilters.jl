b348c460ca961ff1fcf7ef03e1cfcb5a2759f06f
Soeren Schoenbrod <soeren.schoenbrod@outlook.de>
2022-12-04 14:10:57 +0100

    Let SRAUKF support Complex values and StaticArrays

--- test/sraukf.jl
+++ test/sraukf.jl
@@ -1,13 +1,14 @@
 @testset "Square root Augmented Unscented Kalman filter" begin
-    @testset "time update" begin
-        x = randn(6)
-        A = randn(6,6)
+
+    @testset "Time update with $T type $t" for T = (Float64, ComplexF64), t = ((vec = Vector, mat = Matrix), (vec = SVector{3}, mat = SMatrix{3,3}))
+        x = t.vec(randn(T, 3))
+        A = t.mat(randn(T, 3, 3))
         P = A'A
-        P_chol = cholesky(P)
-        B = randn(6,6)
+        P_chol = cholesky(Hermitian(P))
+        B = t.mat(randn(T, 3, 3))
         Q = B'B
-        Q_chol = cholesky(Q)
-        F = randn(6,6)
+        Q_chol = cholesky(Hermitian(Q))
+        F = t.mat(randn(T, 3, 3))
         f(x) = F * x
         f(x, noise) = F * x .+ noise
 
@@ -16,24 +17,26 @@
         @test get_covariance(tu_chol) ≈ get_covariance(tu)
         @test get_state(tu_chol) ≈ get_state(tu)
 
-        f!(y, x) = mul!(y, F, x)
-        f!(y, x, noise) = y.= @~ F * x .+ noise
-        tu_inter = SRAUKFTUIntermediate(6)
-        tu_aug_inplace = time_update!(tu_inter, x, P_chol, f!, Augment(Q_chol))
-        @test get_covariance(tu_aug_inplace) ≈ get_covariance(tu)
-        @test get_state(tu_aug_inplace) ≈ get_state(tu)
+        if x isa Vector
+            f!(y, x) = mul!(y, F, x)
+            f!(y, x, noise) = y.= @~ F * x .+ noise
+            tu_inter = SRAUKFTUIntermediate(T, 3)
+            tu_aug_inplace = time_update!(tu_inter, x, P_chol, f!, Augment(Q_chol))
+            @test get_covariance(tu_aug_inplace) ≈ get_covariance(tu)
+            @test get_state(tu_aug_inplace) ≈ get_state(tu)
+        end
     end
 
-    @testset "measurement update" begin
-        x = randn(6)
-        A = randn(6,6)
+    @testset "Measurement update with $T type $t" for T = (Float64, ComplexF64), t = ((vec = Vector, mat = Matrix), (vec = SVector{3}, mat = SMatrix{3,3}))
+        x = t.vec(randn(T, 3))
+        A = t.mat(randn(T, 3, 3))
         P = A'A
-        P_chol = cholesky(P)
-        B = randn(3,3)
+        P_chol = cholesky(Hermitian(P))
+        B = t.mat(randn(T, 3, 3))
         R = B'B
-        y = randn(3)
-        R_chol = cholesky(R)
-        H = randn(3,6)
+        y = t.vec(randn(T, 3))
+        R_chol = cholesky(Hermitian(R))
+        H = t.mat(randn(T, 3, 3))
         h(x) = H * x
         h(x, noise) = H * x .+ noise
 
@@ -42,11 +45,13 @@
         @test get_covariance(mu) ≈ get_covariance(mu_chol)
         @test get_state(mu) ≈ get_state(mu_chol)
 
-        h!(y, x) = mul!(y, H, x)
-        h!(y, x, noise) = y .= @~ H * x .+ noise
-        mu_inter = @inferred SRAUKFMUIntermediate(6, 3)
-        mu_ukf_inplace = measurement_update!(mu_inter, x, P_chol, y, h!, Augment(R_chol))
-        @test get_covariance(mu_ukf_inplace) ≈ get_covariance(mu)
-        @test get_state(mu_ukf_inplace) ≈ get_state(mu)
+        if x isa Vector
+            h!(y, x) = mul!(y, H, x)
+            h!(y, x, noise) = y .= @~ H * x .+ noise
+            mu_inter = @inferred SRAUKFMUIntermediate(T, 3, 3)
+            mu_ukf_inplace = measurement_update!(mu_inter, x, P_chol, y, h!, Augment(R_chol))
+            @test get_covariance(mu_ukf_inplace) ≈ get_covariance(mu)
+            @test get_state(mu_ukf_inplace) ≈ get_state(mu)
+        end
     end
 end
